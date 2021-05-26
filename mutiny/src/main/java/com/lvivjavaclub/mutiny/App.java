bf23dfc78c2d2ed85e362ff68b122b7a29c57d6a
Ihor Banadiga <banadiga@gmail.com>
2021-05-27 02:47:04 +0300

    init

---
+++ mutiny/src/main/java/com/lvivjavaclub/mutiny/App.java
@@ -0,0 +1,8 @@
+package com.lvivjavaclub.mutiny;
+
+public class App {
+  public static void main(String[] args) {
+
+
+  }
+}
---
+++ quarkus/src/test/java/com/lvivjavaclub/NativequarkusIT.java
@@ -0,0 +1,9 @@
+package com.lvivjavaclub;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativequarkusIT extends quarkusTest {
+
+    // Execute the same tests but in native mode.
+}
